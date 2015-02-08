1238a59a9d347e32d009e38d9bbb6f62f0cfac45
TJ Horner <me@tjhorner.com>
2015-02-07 21:53:01 -0800

    Asd'asd'fsd'f

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var prompt = require('prompt');
+launchpadName = "";
 
 prompt.start();
 
-prompt.get(['ip'], function(e, r){
+prompt.get(['ip', 'name'], function(e, r){
+  launchpadName = r.name;
   socket = require('socket.io-client')(r.ip || "ws://158.130.165.151:3000");
   // console.log(r.ip);
 
@@ -16,49 +18,62 @@ prompt.get(['ip'], function(e, r){
 
     socket.on('device:select', function(){
       console.log('sending launchpad info');
-      prompt.get(['name'], function(e, r){
-        socket.emit('device:select', {id: 'launchpad', name: r.name});
+      socket.emit('device:select', {id: 'launchpad', name: launchpadName});
 
-        var launchpad = require('midi-launchpad').connect(0, false);
+      var launchpad = require('midi-launchpad').connect(0, false);
+      var midi = require('midi');
 
-        launchpad.on('ready', function(launchpad){
-          // clear before anything, it breaks toggle if not cleared
-          launchpad.clear();
-          launchpad.allLight(launchpad.colors.green.high);
+      // set up raw midi if user doesn't want to programmatically do shit
+      // for example, this would be useful in FL Studio/Ableton Live
+      var output = new midi.output();
+      output.openPort(0);
+      socket.on('midi:message', function(msg){
+        output.sendMessage(msg);
+      });
 
-          // START LAUNCHPAD TRIGGERS
-          launchpad.on('press', function(button){
-            socket.emit('button:press', {x: button.x, y: button.y});
-          });
+      var input = new midi.input();
+      input.on('message', function(delta, msg){
+        socket.emit('midi:message', msg);
+      });
+      input.openPort(0);
 
-          launchpad.on('release', function(button){
-            socket.emit('button:release', {x: button.x, y: button.y});
-          });
-          // END LAUNCHPAD TRIGGERS
+      launchpad.on('ready', function(launchpad){
+        // clear before anything, it breaks toggle if not cleared
+        launchpad.clear();
+        // launchpad.allLight(launchpad.colors.green.high);
 
-          // START LAUNCHPAD ACTIONS
-          socket.on('button:light', function(d){
-            launchpad.getButton(d.x, d.y).light(d.color);
-          });
-
-          socket.on('button:off', function(d){
-            launchpad.getButton(d.x, d.y).dark();
-          });
-
-          socket.on('button:toggle', function(d){
-            var button = launchpad.getButton(d.x, d.y);
-            if(button.getState() === 0){
-              button.light(d.color || 3);
-            }else{
-              button.dark();
-            }
-          });
-
-          socket.on('button:light:all', function(d){
-            launchpad.allLight(d.color || 3);
-          });
-          // END LAUNCHPAD ACTIONS
+        // START LAUNCHPAD TRIGGERS
+        launchpad.on('press', function(button){
+          socket.emit('button:press', {x: button.x, y: button.y});
         });
+
+        launchpad.on('release', function(button){
+          socket.emit('button:release', {x: button.x, y: button.y});
+        });
+        // END LAUNCHPAD TRIGGERS
+
+        // START LAUNCHPAD ACTIONS
+        socket.on('button:light', function(d){
+          launchpad.getButton(d.x, d.y).light(d.color);
+        });
+
+        socket.on('button:off', function(d){
+          launchpad.getButton(d.x, d.y).dark();
+        });
+
+        socket.on('button:toggle', function(d){
+          var button = launchpad.getButton(d.x, d.y);
+          if(button.getState() === 0){
+            button.light(d.color || 3);
+          }else{
+            button.dark();
+          }
+        });
+
+        socket.on('button:light:all', function(d){
+          launchpad.allLight(d.color || 3);
+        });
+        // END LAUNCHPAD ACTIONS
       });
     });
 
